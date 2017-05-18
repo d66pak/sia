3b746d6caf6af3709f53cf8e6342174ce69bb932
d66pak <d66pak@users.noreply.github.com>
2017-05-18 16:59:34 +1000

    chapter 3

---
+++ pyspark-notebook.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ABSOLUTE=$(realpath $0)
+WORK_DIR=$(dirname ${ABSOLUTE})
+
+export OUT_DIR=${WORK_DIR}/dataOut
+export IN_DIR=${WORK_DIR}/dataIn
+
+# VIRTUAL_ENV variable will be set if virtual env is activated.
+if [ -z $VIRTUAL_ENV ];
+then
+	echo "Loading python virtualenv..."
+	source ${WORK_DIR}/sparkvenv/bin/activate
+fi
+
+# Only required if SPARK_HOME is not set and added to PATH
+#export SPARK_HOME=/opt/spark
+#export PATH=$PATH:$SPARK_HOME/bin
+
+export PYSPARK_DRIVER_PYTHON=ipython
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
+
+pyspark \
+  --master local[*] \
+  --driver-memory 2g 
